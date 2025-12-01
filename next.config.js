ee6f8b8908e73b1349d442868cfeb7a94b826d54
Kym Feltus <kymfeltus@your-email.com>
2025-11-30 22:19:30 -0600

    Remove redirect so flash page loads

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/feed',
-        permanent: true,
-      },
-    ];
+    return [];
   },
 };
 
