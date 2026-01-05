c7a694a4aa056b8bca6549975b0741b9a47ba6c2
Kym Feltus <kymfeltus@gmail.com>
2026-01-05 01:14:52 -0600

    Add unmistakable debug route

---
+++ app/debug/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+export default function DebugPage() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "red",
+      color: "white",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "32px",
+      fontWeight: "bold"
+    }}>
+      🔥 DEBUG ROUTE IS LIVE 🔥
+    </div>
+  );
+}
